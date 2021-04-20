211aeb1402ae7b4e7bedc14f199d648ae22655ba
Ellis Yoo <yjoo.eyoo@gmail.com>
2021-04-20 16:03:35 -0700

    change logger test

--- __tests__/logger.test.js
+++ __tests__/logger.test.js
@@ -1,11 +1,32 @@
-"use strict";
+const loggerMiddleware = require('../src/middleware/logger.js');
 
-const { server } = require("../src/server");
-const supertest = require("supertest");
-const mockReq = supertest(server);
+// Tested middleware needs to either be exported from the server or a separate module
+describe('logger middleware', () => {
 
-describe("LOGGER TEST", () => {
-  it("placeholder so npm test doesn't cause error",()=>{
-      expect(true).toEqual(true)
+  let consoleSpy;
+  let req = {};
+  let res = {};
+  let next = jest.fn(); // spy on next method
+
+  beforeEach(() => {
+    // Attach to the console (take it over)
+    consoleSpy = jest.spyOn(console, 'log').mockImplementation();
   });
+
+  afterEach(() => {
+    // Put the console back
+    consoleSpy.mockRestore();
+  });
+
+  it('properly logs some output', () => {
+    loggerMiddleware(req, res, next);
+    expect(consoleSpy).toHaveBeenCalled();
+  });
+
+  it('properly moves to the next middleware', () => {
+    loggerMiddleware(req, res, next);
+    // toHaveBeenCalled() is not enough, we need to make sure it was called with no args
+    expect(next).toHaveBeenCalledWith();
+  });
+
 });
